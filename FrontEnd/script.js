45b02c17af6153ca1fb2a754bd98bf9847f31236
matthieu-h <matthieu.hatinguais@hotmail.fr>
2023-05-24 10:49:21 +0200

    ajout de tous les éléments de gallery en dynamique

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,17 +1,16 @@
-const figureOne = document.querySelector("#gallery__figure-one");
-
 fetch("http://localhost:5678/api/works")
   .then((response) => response.json())
-  .then((response2) => {
-    const img = document.createElement("img");
-    img.src = response2[0].imageUrl;
-    figureOne.appendChild(img);
-  });
+  .then((gallery) => {
+    for (let i in gallery) {
+      const figureElement = document.createElement("figure");
+      const imgElement = document.createElement("img");
+      imgElement.src = gallery[i].imageUrl;
+      const figcaptionElement = document.createElement("figcaption");
+      figcaptionElement.innerText = gallery[i].title;
 
-fetch("http://localhost:5678/api/works")
-  .then((response) => response.json())
-  .then((response2) => {
-    const figcaption = document.createElement("figcaption");
-    figcaption.innerText = response2[0].title;
-    figureOne.appendChild(figcaption);
+      const divGallery = document.querySelector(".gallery");
+      divGallery.appendChild(figureElement);
+      figureElement.appendChild(imgElement);
+      figureElement.appendChild(figcaptionElement);
+    }
   });
